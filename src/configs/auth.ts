35fab753f937da1fccca7af2f741d67b4acbae84
Obedkov Vladislav <230765@astanait.edu.kz>
2023-10-10 18:20:18 +0600

    Deploy

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -39,7 +39,6 @@ export const authConfig: NextAuthOptions = {
                     }
                 })
                 if(res.status == 401 || res.status == 404 || res.status == 500) {
-                    console.log(res.statusText);
                     return null;
                 }
                 const user = await res.json();
@@ -63,53 +62,3 @@ export const authConfig: NextAuthOptions = {
        }
     }
 };
-
-// import { NextAuthOptions } from "next-auth";
-// import NextAuth from "next-auth/next";
-// import CredentialsProvider from "next-auth/providers/credentials";
-
-// export const authConfig: NextAuthOptions = {
-//   providers: [
-//     CredentialsProvider({
-//       name: "Credentials",
-//       credentials: {
-//         username: { label: "Username", type: "text" , required: true},
-//         password: { label: "Password", type: "password", required: true },
-//       },
-//       async authorize(credentials, req) {
-//         const res = await fetch("http://localhost:3000/api/admin/login", {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify({
-//             username: credentials?.username,
-//             password: credentials?.password,
-//           }),
-//         });
-
-//         if (res.status === 401) return null;
-//         const user = await res.json();
-
-//         if (user) {
-//           return user;
-//         } else {
-//           return null;
-//         }
-//       },
-//     }),
-//   ],
-//   callbacks: {
-//     async jwt({ token, user, trigger, session }) {
-//       if (trigger === "update") {
-//         return { ...token, ...session.user };
-//       }
-//       return { ...token, ...user };
-//     },
-
-//     async session({ session, token }) {
-//       session.user = token as any;
-//       return session;
-//     },
-//   },
-// };
