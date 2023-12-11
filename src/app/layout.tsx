a9daf5aa983e01809e03a8f84f4283b3f590653d
Obedkov Vladislav <230765@astanait.edu.kz>
2023-12-11 16:12:59 +0600

    Changed gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,19 +51,10 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.setAttribute('src',
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl);f.parentNode?.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5HGJB23C');`
-          }}
-        />
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5HGJB23C"
+        height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe></noscript>
       </head>
       <body className={montserratSemi.className}>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5HGJB23C"
-        height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe></noscript>
         <Providers>
           <div className="helper">
             <div className="content">
