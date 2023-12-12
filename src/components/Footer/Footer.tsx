7258357c6426fffadb959ebb14e145a1a00c0ec7
Obedkov Vladislav <230765@astanait.edu.kz>
2023-12-12 14:56:01 +0600

    Fixed logo format

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import Image from "next/image"
 import { SocialMedia } from "../Header/SocialMedia/SocialMedia"
-import logo from "../../../public/logo.png"
+import logo from "@/assets/logo.png"
 import { UpButton } from "../UI/UpButton"
 import styles from "./Footer.module.scss"
 import cn from 'clsx';
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import logo from "../../../public/logo.png"
+import logo from "@/assets/logo.png"
 import { Burger } from "./Burger/Burger"
 import { SocialMedia } from "./SocialMedia/SocialMedia"
 import { useSession } from "next-auth/react"
