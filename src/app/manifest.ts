fadd6ca886c35e24c53e084b31fc1fa611c60669
Obedkov Vladislav <230765@astanait.edu.kz>
2024-02-04 13:52:14 +0600

    Added maskable icon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -20,6 +20,12 @@ export default function manifest(): MetadataRoute.Manifest {
             sizes: "512x512",
             type: "image/png",
         },
+        {
+            src: "/icon512_maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+        }
         ],
     };
 }
