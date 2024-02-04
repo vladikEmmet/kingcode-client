12dd3ad1541d38b00907ff5e98283caf60db9391
Obedkov Vladislav <230765@astanait.edu.kz>
2024-02-04 13:45:17 +0600

    Added manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: "IT-школа King Code",
+        short_name: "King Code",
+        description: `"King Code" - онлайн-школа для детей в возрасте от 5 до 15 лет. У нас Вы легко сможете изучить математику, программирование и 3D-моделирование с опытными преподавателями. 0% скуки, 100% драйва.`,
+        start_url: "/",
+        display: "standalone",
+        background_color: "#fdd034",
+        theme_color: "#2d2d2d",
+        icons: [
+        // {
+        //     src: "/icon-192x192.png",
+        //     sizes: "192x192",
+        //     type: "image/png",
+        // },
+        {
+            src: "/icon512_rounded.png",
+            sizes: "512x512",
+            type: "image/png",
+        },
+        ],
+    };
+}
